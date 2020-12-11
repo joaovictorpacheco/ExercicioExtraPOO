ad1165feb74256cf5c4c54eef8f569389ea0ee8e
FelipeMarini <felipemarini71188@gmail.com>
2020-12-11 11:03:55 -0300

    Exercício adicionado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercícioExtra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ classes/Elevador.cs
@@ -0,0 +1,40 @@
+namespace ExercícioExtra.classes
+{
+    public class Elevador
+    {
+        public int totalAndares{
+            get{
+                return totalAndares;
+            }
+            set{
+                totalAndares = 4;
+            }
+        }
+        public int andarAtual{
+            get{
+                return andarAtual;
+            }
+            set{
+                 andarAtual = 0;
+            }
+        }
+        public int capacidadeElevador{
+            get{
+                return capacidadeElevador;
+            }
+            set{
+                capacidadeElevador = 8;
+            }
+        }
+        public int pessoasElevador{
+            get{
+                return pessoasElevador;
+            }
+            set{
+                pessoasElevador = 3;
+            }
+        }
+
+
+    }
+}
